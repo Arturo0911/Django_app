066d6bddb9cd540b793e29d0aad219d76d65d878
Arturo <anegreiross@outlook.com>
2020-08-18 18:15:56 -0500

    updated

--- web_project/views.py
+++ web_project/views.py
@@ -3,20 +3,19 @@ from django.shortcuts import render, HttpResponse
 
 def Home(request):
 
-    return HttpResponse("<h1>Home page</h1>")
-
+    return render(request, 'web_project/home.html')
 def Services(request):
 
-    return HttpResponse("<h1>Service page</h1>")
+    return render(request, 'web_project/services.html')
 
 def Shop(request):
 
-    return HttpResponse("<h1>Shop page</h1>")
+    return render(request, 'web_project/shop.html')
 
 def Blog(request):
 
-    return HttpResponse("<h1>blog page</h1>")
+    return render(request, 'web_project/blog.html')
 
 def Contact(request):
 
-    return HttpResponse("<h1>Contact page</h1>")
+    return render(request, 'web_project/contact.html')
