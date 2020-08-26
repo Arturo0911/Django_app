345eefc79a2e2fdce6d5009b2380947f7afbbdcf
Arturo <anegreiross@outlook.com>
2020-08-26 12:50:53 -0500

    ok

--- web_project/static/web_project/js/chart.js
+++ web_project/static/web_project/js/chart.js
@@ -1,38 +1,51 @@
 $(document).ready(function(){
     //alert('hola mundo');
     const ctx = document.getElementById('Mychart').getContext('2d');
-    const myChart = new Chart(ctx, {
-        type: 'bar',
+    var scatterChart = new Chart(ctx, {
+        type: 'scatter',
         data: {
-            labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
             datasets: [{
-                label: '# of Votes',
-                data: [12, 19, 3, 5, 2, 3],
-                backgroundColor: [
-                    'rgba(255, 99, 132, 0.2)',
-                    'rgba(54, 162, 235, 0.2)',
-                    'rgba(255, 206, 86, 0.2)',
-                    'rgba(75, 192, 192, 0.2)',
-                    'rgba(153, 102, 255, 0.2)',
-                    'rgba(255, 159, 64, 0.2)'
-                ],
-                borderColor: [
-                    'rgba(255, 99, 132, 1)',
-                    'rgba(54, 162, 235, 1)',
-                    'rgba(255, 206, 86, 1)',
-                    'rgba(75, 192, 192, 1)',
-                    'rgba(153, 102, 255, 1)',
-                    'rgba(255, 159, 64, 1)'
-                ],
-                borderWidth: 1
+                label: 'Scatter Dataset',
+                backgroundColor: 'rgb(255, 99, 132)',
+                borderColor: 'rgb(255, 99, 132)',
+                data: [{
+                    x: 3,
+                    y: 5
+                }, {
+                    x: 7,
+                    y: 8
+                }, {
+                    x: 8,
+                    y: 8
+                },{
+                    x: 9,
+                    y: 10
+                },{
+                    x: 4,
+                    y: 3
+                },{
+                    x: 2,
+                    y: 2
+                },{
+                    x: 5,
+                    y: 3
+                },{
+                    x: 10,
+                    y: 8
+                },{
+                    x: 8,
+                    y: 9
+                },{
+                    x: 7,
+                    y: 8
+                }]
             }]
         },
         options: {
             scales: {
-                yAxes: [{
-                    ticks: {
-                        beginAtZero: true
-                    }
+                xAxes: [{
+                    type: 'linear',
+                    position: 'bottom'
                 }]
             }
         }
